54770721e0a88c10098408e2d8d9adf5136df282
Esgrid Sikahall <esikahall@gmail.com>
2023-01-24 09:56:06 +0000

    adding what I've got of the identicon project

---
+++ lib/identicon.ex
@@ -0,0 +1,31 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+    This is the main function, where all the process is run.
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_colour
+  end
+
+  @doc """
+    `pick_colour` function receives image and r, g, b and returns a new struct containg image and a tuple: {r, g, b}
+  """
+  def pick_colour(%Identicon.Image{hex: [r, g, b | _]} = image) do
+    %Identicon.Image{image | colour: {r, g, b}}
+  end
+
+  @doc """
+    This is the hashing function. It calls `%Identicon.Image{hex: }` sending `hex` to the struct.
+  """
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+@moduledoc """
+  This is a module that holds defstruct, a struct which holds the data of the identicon.
+"""
+  defstruct hex: nil, colour: nil
+end
